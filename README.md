d6a1f0bc6ce05cbf909e7814b67ed97d3472c7a7
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-10-20 14:06:28 -0500

    added browserify for rtc-everywhere and commented code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # web-rtc
 
-The code previewed here is not my code. It is a tutorial given [here](https://www.twilio.com/blog/2014/12/set-phasers-to-stunturn-getting-started-with-webrtc-using-node-js-socket-io-and-twilios-nat-traversal-service.html) to learn about web-rtc and how it works with web sockets.
+The code previewed here is not my code. It is a tutorial given [here](https://www.twilio.com/blog/2014/12/set-phasers-to-stunturn-getting-started-with-webrtc-using-node-js-socket-io-and-twilios-nat-traversal-service.html) to learn and take notes about web-rtc and how it works with web sockets.
+
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ server.connection({
 let socketio = require("socket.io");
 let io = socketio(server.listener);
 let twilio = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+let wrtc = require('wrtc')
+
 
 // Serve static assets
 server.route({
@@ -20,39 +22,45 @@ server.route({
 });
 
 io.on('connection', function(socket){
-
+  //when someone presses the get video button
+  // could also be online offline
   socket.on('join', function(room){
+    //set up a room for users to join
+    //this needs to be something specific to the user setting it up
     let clients = io.sockets.adapter.rooms[room];
+    //make sure that only two clients in one room
     let numClients = (clients !== undefined) ? clients.length : 0;
     if(numClients == 0){
     	socket.join(room)
     }else if(numClients == 1){
       socket.join(room)
-      socket.emit('ready', room);
-      socket.broadcast.emit('ready', room);
+      //emits room is ready and call can be made
+      io.emit('ready', room);
     }else{
       socket.emit('full', room);
     }
   });
 
   socket.on('token', function(){
+  //request tokens from twilio
     twilio.tokens.create(function(err, response){
       if(err){
         console.log(err);
       }else{
+        //sends response to client side
         socket.emit('token', response);
       }
     });
   });
-
+  //tell everyone a new candidate has joined the server
   socket.on('candidate', function(candidate){
     socket.broadcast.emit('candidate', candidate);
   });
-
+  //broadcasts offer to other browser
   socket.on('offer', function(offer){
     socket.broadcast.emit('offer', offer);
   });
-
+  //original caller receives an answer
   socket.on('answer', function(answer){
     socket.broadcast.emit('answer', answer);
   });
